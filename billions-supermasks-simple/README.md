970a015c61390f73748b77949b45aa6c8c5879db
sonGOKU <tx033devil@gmail.com>
2025-09-09 17:46:50 +0530

    Add files via upload

---
+++ billions-supermasks-simple/README.md
@@ -0,0 +1,5 @@
+Billions Supermasks Simple Starter - Plain CSS version
+
+1. Extract files.
+2. Run `npm install`.
+3. Run `npm run dev` to start local dev server.
---
+++ billions-supermasks-simple/pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Billions Supermasks</h1>
+      <p>Simple CSS starter project deployed on Vercel</p>
+      <a className="button" href="https://vercel.com" target="_blank" rel="noreferrer">
+        Deploy on Vercel
+      </a>
+    </div>
+  )
+}
